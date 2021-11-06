fab3da9aeb64fd21d8152c0fcbae173986eb6439
bettyvng <betty.vg8@gmail.com>
2021-11-06 21:57:15 +0000

    sum the largest numbers

---
+++ kata1.js
@@ -0,0 +1,8 @@
+const sumLargestNumbers = function(data) {
+  numArr.sort((a, b) => b - a);
+    return numArr[0] + numArr[1];
+  };
+  
+  console.log(sumLargestNumbers([1, 10]));
+  console.log(sumLargestNumbers([1, 2, 3]));
+  console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
