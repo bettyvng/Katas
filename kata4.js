ff47e8385c7f82370e97300d273854bfa55c3c61
bettyvng <betty.vg8@gmail.com>
2021-11-07 21:58:52 +0000

    instructors name

---
+++ kata4.js
@@ -0,0 +1,23 @@
+const instructorWithLongestName = function(data) {
+  let longestLength = 0;
+  let longestLengthIndex = 0;
+  for (let i = 0; i < data.length; i++){
+    if((data[i].name).length > longestLength){
+      longestLength = (data[i].name).length;
+      longestLengthIndex = i;
+    }
+  }
+  return data[longestLengthIndex];
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
